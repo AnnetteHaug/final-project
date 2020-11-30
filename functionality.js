1ca65ec44a643899b7c59005efb53cfb39bdfde8
Skandinaver <73300137+Skandinaver@users.noreply.github.com>
2020-11-30 12:17:00 +0100

    Add files via upload

---
+++ functionality.js
@@ -0,0 +1,8 @@
+
+function uploadSheetPresent() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function uploadSheetdismiss() {
+    document.getElementById("overlay").style.display = "none";
+  }
